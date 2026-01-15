bbf7f13d1b342f2ed1b823255ad7adebeace427a
Rascal <faiqemilzzz@gmail.com>
2026-01-15 17:23:59 +0700

    Improving eww 0.1

--- eww/scripts/music.sh
+++ eww/scripts/music.sh
@@ -1,15 +1,29 @@
-title=$(mpc --format "%title%" current 2>/dev/null)
-file=$(mpc --format "%file%" current)
-artist=$(mpc --format "%artist%" current 2>/dev/null)
-# status=$(mpc status 2>/dev/null)
+arg=$1
 
-if [[ -z "$file" ]]; then
-  echo "No Music"
-  exit
-elif [[ -z "$title" ]]; then
-  base="${file%.*}"
-  artist="${base%% - *}"
-  title="${base#* - }"
+get_music_info() {
+  title=$(mpc --format "%title%" current 2>/dev/null)
+  file=$(mpc --format "%file%" current)
+  artist=$(mpc --format "%artist%" current 2>/dev/null)
+
+  if [[ -z "$file" ]]; then
+    echo "No Music"
+    exit
+  elif [[ -z "$title" ]]; then
+    base="${file%.*}"
+    artist="${base%% - *}"
+    title="${base#* - }"
+  fi
+
+  echo "$artist - $title"
+}
+
+get_music_status() {
+  echo "$(mpc status | awk 'NR==2 {print $1}' | tr -d '[]')"
+}
+
+if [[ $arg == "status" ]]; then
+  get_music_status
+else
+    get_music_info
 fi
 
-echo "$artist - $title"
