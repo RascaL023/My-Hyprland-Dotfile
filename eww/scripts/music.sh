a621451cf53a50d7bb1da2e145bb4653fc96e124
Rascal <faiqemilzzz@gmail.com>
2026-01-16 23:15:05 +0700

    Updating eww 0.3 - add cover art

--- eww/scripts/music.sh
+++ eww/scripts/music.sh
@@ -6,7 +6,7 @@ get_music_info() {
   artist=$(mpc --format "%artist%" current 2>/dev/null)
 
   if [[ -z "$file" ]]; then
-    echo "No Music "
+    echo "No Music"
     exit
   elif [[ -z "$title" ]]; then
     base="${file%.*}"
@@ -18,11 +18,51 @@ get_music_info() {
 }
 
 get_music_status() {
-  echo "$(mpc status | awk 'NR==2 {print $1}' | tr -d '[]' || 'No Music-')"
+  echo "$(mpc status | awk 'NR==2 {print $1}' | tr -d '[]' || 'No Music')"
 }
+
+get_music_cover() {
+  MUSIC_DIR="$HOME/Shared/Music"
+  COVER_DIR="$HOME/Shared/.cover"
+  FALLBACK_COVER="$HOME/Pictures/Icons/pochita.icon"
+
+  current_file=$(mpc -f %file% current)
+
+  if [ -z "$current_file" ]; then
+      echo "$FALLBACK_COVER"
+      exit
+  fi
+
+  full_path="$MUSIC_DIR/$current_file"
+
+  cover_filename=$(ffprobe -v error -select_streams v:0 -show_entries stream_tags=title -of default=noprint_wrappers=1:nokey=1 "$full_path" 2>/dev/null)
+
+  if [ -n "$cover_filename" ]; then
+      cover_path="$COVER_DIR/$cover_filename"
+      
+      if [ -f "$cover_path" ]; then
+          echo "$cover_path"
+          return
+      fi
+  else
+    echo "$FALLBACK_COVER"
+  fi
+}
+
+
 
 if [[ $arg == "status" ]]; then
   get_music_status
+
+  mpc idleloop player | while read -r; do
+    get_music_status
+  done
+elif [[ $arg == "cover" ]]; then
+  get_music_cover
+
+  mpc idleloop player | while read -r; do
+      get_music_cover
+  done
 else
     get_music_info
 fi
