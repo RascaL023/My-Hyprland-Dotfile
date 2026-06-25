9077e747c89089367e24d1e4d98567e885e10ef1
Rascal <faiqemilzzz@gmail.com>
2026-06-26 01:31:10 +0700

    feat(options): add indent rule on json file set to be 2

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -19,3 +19,13 @@ opt.undofile = true --- location: ~/.local/state/nvim/undo/
 vim.o.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
 vim.env.PATH = vim.env.PATH .. ":" .. vim.fn.stdpath("data") .. "/mason/bin"
 
+-- Rule: json indent is 2
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+  end,
+})
+
