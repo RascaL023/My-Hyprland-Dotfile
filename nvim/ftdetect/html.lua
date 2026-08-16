8fc890634f3809bea869597372f114dc16d7b296
Rascal <faiqemilzzz@gmail.com>
2026-08-16 07:12:58 +0700

    refactor(nvim): move json/jsonc tab config to ftdetect, add htm

--- nvim/ftdetect/html.lua
+++ nvim/ftdetect/html.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
-  pattern = "*.html",
+  pattern = { "*.html", "*.htm" },
   command = "set filetype=html"
 })
---
+++ nvim/ftplugin/json.lua
@@ -0,0 +1,5 @@
+-- JSON memakai indentasi 2 spasi
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.softtabstop = 2
